3e823bcf9c7de15c8a09df5a6a9e7a9bf024cc28
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-03-12 12:17:55 -0400

    replacing default html elements for styled elements and adding metadata

--- src/app/_home/styles.ts
+++ src/app/_home/styles.ts
@@ -2,12 +2,12 @@
 import styled from "styled-components";
 
 export const Layout = styled.main`
-  width: 100vw;
   padding: 1rem;
   height: 100vh;
   display: grid;
-  /* grid-template-columns: 200px auto; */
-  background-color: #1e1e1e;
+  max-width: 1300px;
+  margin: 0 auto;
+  grid-template-columns: 400px auto;
 `;
 
 export const Main = styled.section`
@@ -15,3 +15,7 @@ export const Main = styled.section`
   flex-direction: column;
   gap: 2rem;
 `;
+
+export const Html = styled.html``;
+
+export const Body = styled.body``;
