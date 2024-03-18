7a8e013a4b33265b067ac9f51e022ffd6b3c6731
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-03-18 16:58:33 -0400

    working on feedbacks page

--- src/app/(base)/feedbacks/page.tsx
+++ src/app/(base)/feedbacks/page.tsx
@@ -1,5 +1,43 @@
-import {Typography} from "@/src/components";
+import {Fragment} from "react";
+import {Heart} from "react-feather";
+import {Feedback, Row, Typography} from "@/src/components";
+import {Feedback as FeedbackModel} from "@/src/interfaces";
+
+const mock: FeedbackModel[] = [
+  {
+    user: {
+      firstName: "Luis",
+      lastName: "Sanchez",
+      role: "Senior Backend Developer",
+      avatar:
+        "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    feedback:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, itaque!",
+  },
+  {
+    user: {
+      firstName: "Pedro",
+      lastName: "Martinez",
+      role: "Senior DevOps",
+      avatar:
+        "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    feedback:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis quasi illo unde obcaecati magnam fugiat aspernatur nesciunt neque ratione cupiditate.",
+  },
+];
 
 export default function Feedbacks() {
-  return <Typography>Feedbacks</Typography>;
+  return (
+    <Fragment>
+      <Row gap={10}>
+        <Typography variant="title">Thank you all</Typography>
+        <Heart color="red" fill="red" />
+      </Row>
+      {mock.map((feedback, key) => (
+        <Feedback key={key} feedback={feedback} />
+      ))}
+    </Fragment>
+  );
 }
