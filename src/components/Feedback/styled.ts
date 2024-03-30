78da075881feb4287fe36af463194f4ab498c77f
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-03-30 09:08:01 -0400

    updating feedback style

--- src/components/Feedback/styled.ts
+++ src/components/Feedback/styled.ts
@@ -2,7 +2,7 @@ import {theme} from "@/src/styles";
 import styled from "styled-components";
 
 export const Message = styled.div`
-  padding: 0.8rem;
+  padding: 0.5rem;
   max-width: 450px;
   border-radius: 5px;
   background-color: ${theme["secondary"]};
